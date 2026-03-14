cede31673d036fc332312ce7c014b0cb30479202
nnymph <265343938+nnymph@users.noreply.github.com>
2026-03-14 04:06:15 +0300

    Исправил баг и обновил код сборки на windows

--- package-release-zip.sh
+++ package-release-zip.sh
@@ -1,55 +1,91 @@
 #!/bin/bash
 
+# Останавливаем скрипт при любой ошибке, необъявленной переменной или сбое в пайплайне
+set -euo pipefail
+
 Arch="$1"
 OutputPath="$2"
 
 OutputArch="v2rayN-${Arch}"
 FileName="v2rayN-${Arch}.zip"
-
-# 1. Скачиваем оригинальный бандл
-wget -nv -O $FileName "https://github.com/2dust/v2rayN-core-bin/raw/refs/heads/master/$FileName"
-
 ZipPath64="./$OutputArch"
-mkdir -p "$ZipPath64"
+BinPath="$ZipPath64/$OutputArch/bin"
+
+# Базовые ссылки (чтобы не дублировать их в коде)
+ASSET_REPO_LATEST="https://github.com/nnymph/v2ray-rules-dat/releases/latest/download"
+GEOIP_MMDB_URL="https://raw.githubusercontent.com/nnymph/v2ray-ru-blocked-geoip/release/Country.mmdb"
+METADB_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/latest/download/geoip.metadb"
+SRS_BASE_URL="https://raw.githubusercontent.com/nnymph/v2ray-rules-dat/release/sing-box"
+
+echo "[*] Начинаем сборку для архитектуры: $Arch"
+
+# 1. Скачиваем оригинальный бандл (используем curl вместо wget для единообразия)
+echo "[+] Скачиваем оригинальное ядро v2rayN-core-bin..."
+curl -fsSL -o "$FileName" "https://github.com/2dust/v2rayN-core-bin/raw/refs/heads/master/$FileName"
 
 # 2. Распаковываем
-unzip -q $FileName -d "$ZipPath64"
+mkdir -p "$ZipPath64"
+unzip -q "$FileName" -d "$ZipPath64"
 
 # 3. Копируем твой скомпилированный GUI
-cp -rf $OutputPath/* "$ZipPath64/$OutputArch/"
+echo "[+] Копируем скомпилированные файлы GUI..."
+cp -rf "$OutputPath"/* "$ZipPath64/$OutputArch/"
 
-# 4. ПЕРЕХОДИМ К ЗАМЕНЕ ФАЙЛОВ
-cd "$ZipPath64/$OutputArch"
+# 4. Создаем директории для гео-баз
+mkdir -p "$BinPath/srss"
 
-echo "[+] Overwriting Geo assets with nnymph versions..."
-# Скачиваем с принудительной перезаписью (-o)
-curl -fsSL -o "geosite.dat" "https://github.com/nnymph/v2ray-rules-dat/releases/latest/download/geosite.dat"
-curl -fsSL -o "geoip.dat" "https://github.com/nnymph/v2ray-rules-dat/releases/latest/download/geoip.dat"
-curl -fsSL -o "Country.mmdb" "https://raw.githubusercontent.com/nnymph/v2ray-ru-blocked-geoip/release/Country.mmdb"
-curl -fsSL -o "geoip.metadb" "https://github.com/MetaCubeX/meta-rules-dat/releases/latest/download/geoip.metadb"
+# 5. Скачиваем основные Geo-базы
+echo "[+] Загружаем основные Geo-файлы в папку bin/..."
+curl -fsSL -o "$BinPath/geosite.dat" "$ASSET_REPO_LATEST/geosite.dat"
+curl -fsSL -o "$BinPath/geoip.dat" "$ASSET_REPO_LATEST/geoip.dat"
+curl -fsSL -o "$BinPath/Country.mmdb" "$GEOIP_MMDB_URL"
+curl -fsSL -o "$BinPath/geoip.metadb" "$METADB_URL"
 
-# 5. РАБОТА С SRS ФАЙЛАМИ
-echo "[+] Downloading .srs rule-sets..."
-mkdir -p "srss"
+# 6. Работа с SRS правилами
+echo "[+] Загружаем .srs правила для sing-box..."
 
-# Список GEOIP SRS
-for f in geoip-discord.srs geoip-nnymph-all-blocked.srs geoip-nnymph-block.srs \
-         geoip-nnymph-direct.srs geoip-nnymph-proxy.srs geoip-private.srs geoip-ru-blocked.srs; do
-    curl -fsSL -o "srss/$f" "https://raw.githubusercontent.com/nnymph/v2ray-rules-dat/release/sing-box/rule-set-geoip/$f"
+# Исправленный массив GEOIP (сюда переехали cloudflare, cloudfront, fastly)
+GEOIP_RULES=(
+    "geoip-discord.srs"
+    "geoip-nnymph-all-blocked.srs"
+    "geoip-nnymph-block.srs"
+    "geoip-nnymph-direct.srs"
+    "geoip-nnymph-proxy.srs"
+    "geoip-private.srs"
+    "geoip-ru-blocked.srs"
+    "geoip-cloudflare.srs"
+    "geoip-cloudfront.srs"
+    "geoip-fastly.srs"
+)
+
+for f in "${GEOIP_RULES[@]}"; do
+    curl -fsSL -o "$BinPath/srss/$f" "$SRS_BASE_URL/rule-set-geoip/$f"
 done
 
-# Список GEOSITE SRS
-for f in geosite-category-ads-all.srs geosite-discord.srs geosite-google.srs \
-         geoip-cloudflare.srs geoip-cloudfront.srs geoip-fastly.srs \
-         geosite-nnymph-block.srs geosite-nnymph-direct.srs geosite-nnymph-proxy.srs \
-         geosite-private.srs geosite-ru-available-only-inside.srs geosite-nnymph-ru-blocked-self.srs \
-         geosite-twitch-blocked.srs geosite-ru-blocked-self.srs geosite-ru-blocked.srs geosite-youtube.srs; do
-    curl -fsSL -o "srss/$f" "https://raw.githubusercontent.com/nnymph/v2ray-rules-dat/release/sing-box/rule-set-geosite/$f"
+# Массив GEOSITE
+GEOSITE_RULES=(
+    "geosite-category-ads-all.srs"
+    "geosite-discord.srs"
+    "geosite-google.srs"
+    "geosite-nnymph-block.srs"
+    "geosite-nnymph-direct.srs"
+    "geosite-nnymph-proxy.srs"
+    "geosite-private.srs"
+    "geosite-ru-available-only-inside.srs"
+    "geosite-nnymph-ru-blocked-self.srs"
+    "geosite-twitch-blocked.srs"
+    "geosite-ru-blocked-self.srs"
+    "geosite-ru-blocked.srs"
+    "geosite-youtube.srs"
+)
+
+for f in "${GEOSITE_RULES[@]}"; do
+    curl -fsSL -o "$BinPath/srss/$f" "$SRS_BASE_URL/rule-set-geosite/$f"
 done
 
-# Возвращаемся в корень для упаковки
-cd ../../
+# 7. Финальная упаковка
+echo "[+] Создаем итоговый архив $FileName..."
+rm -f "$FileName"
+7z a -tZip "$FileName" "$ZipPath64/$OutputArch" -mx1
 
-# 6. Финальная упаковка
-rm -f $FileName
-7z a -tZip $FileName "$ZipPath64/$OutputArch" -mx1
+echo "[*] Сборка успешно завершена!"
